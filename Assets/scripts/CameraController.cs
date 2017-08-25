cb6a4c0eac2e3de3fdfdbbf05c190777544ebf56
CrawfordD1 <crawforddavidson@gmail.com>
2017-08-25 13:59:18 +0100

    initial commit

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject aim;
+	public GameObject aimLine;
+
+	private Vector3 offset;
+	public float horizontalSpeed = 2.0F;
+	public float verticalSpeed = 2.0F;
+
+	void Start ()
+	{
+		offset = transform.position - aim.transform.position;
+		aim.transform.position = player.transform.position;
+		aimLine.transform.position = aim.transform.position;
+		Screen.lockCursor = true;
+	}
+
+	void LateUpdate ()
+	{
+		aim.transform.position = player.transform.position;
+			float h = horizontalSpeed * Input.GetAxis("Mouse X");
+			float v = verticalSpeed * Input.GetAxis("Mouse Y");
+			aim.transform.Rotate(0, h, 0);
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text winText;
+	private Rigidbody rb;
+	public GameObject aim;
+	public GameObject aimLine;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		aimLine.SetActive (false);
+	}
+
+	void FixedUpdate ()
+	{
+		if (rb.velocity.magnitude < 0.3) {
+			aimLine.SetActive (true);
+			rb.velocity = new Vector3 (0, 0, 0);
+
+			if (Input.GetKeyDown ("up")) {
+				rb.transform.rotation = aim.transform.rotation;
+				rb.AddRelativeForce (Vector3.forward * speed);
+				aimLine.SetActive (false);
+			}
+		}
+
+		if (Input.GetKeyDown ("space")) {
+			rb.AddForce (Vector3.up * speed/2);
+		}
+ }
+		
+
+
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+		}
+		if (other.gameObject.CompareTag("Golf Hole"))
+		{
+			winText.gameObject.SetActive(true);
+//			yield WaitForSeconds(3.0);
+			int scene = SceneManager.GetActiveScene().buildIndex;
+			SceneManager.LoadScene(scene, LoadSceneMode.Single);
+		}
+	}
+
+
+
+
+}
+
+
