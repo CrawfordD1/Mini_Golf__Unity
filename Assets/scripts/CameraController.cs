c923945b0df78d03f03e7ce7cb93ee1b4f97a649
CrawfordD1 <crawforddavidson@gmail.com>
2017-08-25 17:33:30 +0100

    implemented powerBar to shot

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -10,21 +10,18 @@ public class CameraController : MonoBehaviour {
 
 	private Vector3 offset;
 	public float horizontalSpeed = 2.0F;
-	public float verticalSpeed = 2.0F;
-
 	void Start ()
 	{
 		offset = transform.position - aim.transform.position;
 		aim.transform.position = player.transform.position;
 		aimLine.transform.position = aim.transform.position;
-		Screen.lockCursor = true;
+//		Screen.lockCursor = true;
 	}
 
-	void LateUpdate ()
+	void FixedUpdate()
 	{
-		aim.transform.position = player.transform.position;
+			aim.transform.position = player.transform.position;
 			float h = horizontalSpeed * Input.GetAxis("Mouse X");
-			float v = verticalSpeed * Input.GetAxis("Mouse Y");
 			aim.transform.Rotate(0, h, 0);
 	}
 }
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -8,14 +9,20 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public Text winText;
+	public Vector3 aimRotation;
 	private Rigidbody rb;
 	public GameObject aim;
 	public GameObject aimLine;
+	public float mouseInput;
+	public float mouseEndPoint;
+	public bool mouseHeld;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
 		aimLine.SetActive (false);
+		mouseHeld = false;
+		speed = 10;
 	}
 
 	void FixedUpdate ()
@@ -24,27 +31,47 @@ public class PlayerController : MonoBehaviour {
 			aimLine.SetActive (true);
 			rb.velocity = new Vector3 (0, 0, 0);
 
-			if (Input.GetKeyDown ("up")) {
-				rb.transform.rotation = aim.transform.rotation;
+			if (Input.GetMouseButtonDown(0)) {
+				if (mouseHeld) {
+				} else {
+					rb.transform.rotation = aim.transform.rotation;
+					mouseInput = Input.mousePosition.y;
+					mouseHeld = true;
+				}
+			}
+
+			if (mouseHeld) {
+				mouseEndPoint = Input.mousePosition.y;
+				float powerAmount = (mouseEndPoint - mouseInput);
+				if (powerAmount <= 150 && powerAmount >=0) {
+					aimLine.transform.localScale = new Vector3 (75, 30, powerAmount);
+				}
+			}
+
+			if (Input.GetMouseButtonUp (0)) {
+				speed = ((Input.mousePosition.y - mouseInput) * 10 ); 
+				if (speed > 1500) {
+					speed = 1500;
+				}
+				if(speed < 0){
+					speed = 0;
+				}
+
 				rb.AddRelativeForce (Vector3.forward * speed);
 				aimLine.SetActive (false);
+				mouseHeld = false;
+				speed = 10;
+				aimLine.transform.localScale = new Vector3 (75, 30, 70);
 			}
 		}
 
 		if (Input.GetKeyDown ("space")) {
-			rb.AddForce (Vector3.up * speed/2);
+			rb.AddForce (Vector3.up * 400);
 		}
  }
-		
-
-
 
 	void OnTriggerEnter(Collider other) 
 	{
-		if (other.gameObject.CompareTag("Pick Up"))
-		{
-			other.gameObject.SetActive (false);
-		}
 		if (other.gameObject.CompareTag("Golf Hole"))
 		{
 			winText.gameObject.SetActive(true);
@@ -53,6 +80,7 @@ public class PlayerController : MonoBehaviour {
 			SceneManager.LoadScene(scene, LoadSceneMode.Single);
 		}
 	}
+
 
 
 
